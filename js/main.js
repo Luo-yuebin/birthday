dbe3fd0f0ee7b4e6deb03fe4104b1643cee5ced9
Luo-yuebin <38953086+Luo-yuebin@users.noreply.github.com>
2019-03-01 02:15:47 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,84 @@
+$(document).ready(function(){
+
+
+ $('#boy').click(function() {
+ 	$("#boy").removeAttr("click");
+ 	$('#say').text("你干嘛点我呀？");
+ 	$('#boy').click(function() {
+ 		$("#boy").removeAttr("click");
+ 		$('#say').text("你又来点我啦？");
+ 		$('#boy').click(function() {
+ 			$("#boy").removeAttr("click");
+ 			$('#say').text("事不过三哦");
+ 			$('#boy').click(function() {
+ 				$("#boy").removeAttr("click");
+ 				$('#say').text("过三就过三");
+	 			$('#boy').click(function() {
+	 				$("#boy").removeAttr("click");
+	 				$('#say').text("你好看说啥都对");
+	 				$('#boy').click(function() {
+	 					$("#boy").removeAttr("click");
+		 				$('#say').text("给你点");
+		 				$('#boy').click(function() {
+		 					$("#boy").removeAttr("click");
+			 				$('#say').text("再给你点");
+			 				$('#boy').click(function() {
+			 					$("#boy").removeAttr("click");
+				 				$('#say').text("等等！");
+				 				$('#boy').click(function() {
+				 					$("#boy").removeAttr("click");
+					 				$('#say').text("差点忘了说正事");
+					 				$('#boy').click(function() {
+					 					$("#boy").removeAttr("click");
+						 				$('#say').text("今天是你生日啊！");
+						 					$('#boy').attr('src', 'image/02.gif')
+						 					click1();
+						 			});
+					 			});
+				 			});
+			 			});
+		 			});
+	 			});
+ 			});
+ 		});
+ 	});
+ });
+
+
+
+function click1() {
+	$('#boy').click(function() {
+		$("#boy").removeAttr("click");
+		$('#say').text("( •̀ ω •́ )y耶！！！");
+		$('#boy').click(function() {
+			$("#boy").removeAttr("click");
+			$('#say').text("ヽ(✿ﾟ▽ﾟ)ノ普天同庆");
+			$('#boy').click(function() {
+				$("#boy").removeAttr("click");
+				$('#say').text("锣鼓喧天♪(^∇^*)！");
+				$('#boy').click(function() {
+					$("#boy").removeAttr("click");
+					$('#say').text("(/≧▽≦)/鞭炮齐鸣");
+					$('#boy').click(function() {
+						$("#boy").removeAttr("click");
+						$('#say').text("生日快乐！么么哒！");
+						$('#boy').click(function() {
+							$("#boy").removeAttr("click");
+							$('#say').text("再点一下就拜拜咯~");
+							$('#boy').click(function() {
+								$("#boy").removeAttr("click");
+								window.opener = null;    
+		           	 			window.open('', '_self', '');   
+		           				window.close();
+							});
+						});
+					});
+				});
+			});
+		});
+	});
+}
+
+
+
+});
